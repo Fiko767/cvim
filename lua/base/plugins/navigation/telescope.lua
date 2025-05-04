ea2b869fcbc64b79f7baec8d38e582500598ddb5
Fiko767 <767fiko@gmail.com>
2025-05-04 12:21:43 +0200

    fix: it better

--- lua/base/plugins/navigation/telescope.lua
+++ lua/base/plugins/navigation/telescope.lua
@@ -41,7 +41,7 @@ return {
 				},
 			},
 			defaults = {
-				file_ignore_patterns = { ".git/", ".cache/" },
+				file_ignore_patterns = { "%.git/", ".cache/" },
 			},
 			pickers = {
 				find_files = {
