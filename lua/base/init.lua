55991f70a7ef07b2632ffed818eacadccf5d59dc
Fiko767 <767fiko@gmail.com>
2025-05-05 16:05:48 +0200

    feat(user): add a structure for the user folder

--- lua/base/init.lua
+++ lua/base/init.lua
@@ -1,2 +1,1 @@
 require("base.core")
-require("base.plugins")
---
+++ lua/lazy-nvim.lua
@@ -0,0 +1,5 @@
+-- This file is loading the plugins so remove nothing from here
+require("lazy").setup({
+	require("base.plugins"),
+	require("user.plugins"),
+})
